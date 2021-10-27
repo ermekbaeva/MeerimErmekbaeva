68b36c73b1decdfc7b2188e1d28ed8a518874817
ermekbaeva <ermekbaeva@gmail.com>
2021-10-27 12:39:00 +0300

    Sum test edited

--- src/test/java/com/epam/tc/hw1/SumCalculatorTest.java
+++ src/test/java/com/epam/tc/hw1/SumCalculatorTest.java
@@ -2,22 +2,11 @@ package com.epam.tc.hw1;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class SumCalculatorTest extends CalculatorBaseTest {
 
-    @DataProvider
-    public Object[][] sumDataProvider() {
-        return new Object[][]{
-                {23, 2, 25},
-                {89, 10, 99},
-                {12, 90, 102},
-                {12, 67, 79}
-        };
-    }
-
-    @Test(dataProvider = "sumDataProvider")
+    @Test(dataProviderClass = CalculatorDataProvider.class, dataProvider = "sumDataProvider")
     public void sumTest(int a, int b, int expected) {
         System.out.printf("Sum %d + %d test%n", a, b);
         int actual = calculator.sum(a, b);
