bbbb743f71afe2d1eeb95015a169b3843c1c02f2
ermekbaeva <ermekbaeva@gmail.com>
2021-10-27 12:37:49 +0300

    Divizion by zero test added

--- src/test/java/com/epam/tc/hw1/DivisionCalculatorTest.java
+++ src/test/java/com/epam/tc/hw1/DivisionCalculatorTest.java
@@ -1,26 +1,18 @@
 package com.epam.tc.hw1;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DivisionCalculatorTest extends CalculatorBaseTest {
 
-    @DataProvider
-    public Object[][] divisionDataProvider() {
-        return new Object[][]{
-                {6, 2, 3},
-                {110, 10, 11},
-                {27, 9, 3},
-                {81, 9, 9}
-        };
-    }
-
-    @Test(dataProvider = "divisionDataProvider")
+    @Test(dataProviderClass = CalculatorDataProvider.class, dataProvider = "divisionDataProvider")
     public void divisionTest(int a, int b, int expected) {
         System.out.printf("Division %d / %d test%n", a, b);
         int actual = calculator.division(a, b);
         assertThat(actual).isEqualTo(expected);
+        assertThatThrownBy(() -> calculator.division(32, 0))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
