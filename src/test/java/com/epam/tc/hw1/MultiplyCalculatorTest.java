38cc5a5d4da2edd04b338c3c759c73ab336a1543
ermekbaeva <ermekbaeva@gmail.com>
2021-10-27 12:38:20 +0300

    Multiply test edited

--- src/test/java/com/epam/tc/hw1/MultiplyCalculatorTest.java
+++ src/test/java/com/epam/tc/hw1/MultiplyCalculatorTest.java
@@ -2,22 +2,11 @@ package com.epam.tc.hw1;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class MultiplyCalculatorTest extends CalculatorBaseTest {
 
-    @DataProvider
-    public Object[][] multiplyDataProvider() {
-        return new Object[][]{
-                {3, 2, 6},
-                {11, 10, 110},
-                {7, 9, 63},
-                {55, 8, 440}
-        };
-    }
-
-    @Test(dataProvider = "multiplyDataProvider")
+    @Test(dataProviderClass = CalculatorDataProvider.class, dataProvider = "multiplyDataProvider")
     public void multiplyTest(int a, int b, int expected) {
         System.out.printf("Multiply %d * %d test%n", a, b);
         int actual = calculator.multiply(a, b);
