3fac267b51ced7b494216fbda42fc2537ed8a99e
ermekbaeva <ermekbaeva@gmail.com>
2021-11-08 12:31:48 +0300

    base page added

---
+++ src/test/java/com/epam/tc/hw6/pages/BasePage.java
@@ -0,0 +1,36 @@
+package com.epam.tc.hw6.pages;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasePage {
+    public static WebDriver webDriver;
+    public static Properties prop;
+
+    public BasePage() {
+        try {
+            prop = new Properties();
+            FileInputStream ip = new FileInputStream(System.getProperty("user.dir")
+                    + "/src/test/resources/com.epam.tc.hw4/test.properties");
+            prop.load(ip);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void init() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        webDriver.get(prop.getProperty("url"));
+    }
+}
