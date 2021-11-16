aa5b2763e77b739709e4d72a40620ffece4e11fe
ermekbaeva <ermekbaeva@gmail.com>
2021-11-16 13:39:35 +0300

    added homework 1 and 2

---
+++ src/test/java/com/epam/tc/hw1/SumCalculatorTest.java
@@ -0,0 +1,17 @@
+package com.epam.tc.hw1;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.testng.annotations.Test;
+
+public class SumCalculatorTest extends CalculatorBaseTest {
+
+    @Test(dataProviderClass = CalculatorDataProvider.class, dataProvider = "sumDataProvider")
+    public void sumTest(int a, int b, int expected) {
+        System.out.printf("Sum %d + %d test%n", a, b);
+        int actual = calculator.sum(a, b);
+        assertThat(actual)
+                .as("Failed on sum %d and %d", a, b)
+                .isEqualTo(expected);
+    }
+}
