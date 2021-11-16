4cb908d421f3d2afbcab2acb3e1e3c8312ccd1f1
ermekbaeva <ermekbaeva@gmail.com>
2021-11-16 16:15:17 +0300

    added methods for colors

--- src/main/java/com/epam/tc/hw7/sections/MetalsAndColorsForm.java
+++ src/main/java/com/epam/tc/hw7/sections/MetalsAndColorsForm.java
@@ -22,6 +22,14 @@ import org.openqa.selenium.support.ui.Select;
 import java.util.List;
 
 public class MetalsAndColorsForm extends Form<MetalsAndColors> {
+    @FindBy(css = "#vegetables .caret")
+    WebElement vegetablesExpand;
+    @FindBy(css = "#vegetables label")
+    List<WebElement> vegetablesList;
+    @FindBy(css = "#vegetables button")
+    WebElement vegetablesValue;
+    @FindBy(css = "#vegetables ul")
+    WebElement vegetablesIsExpanded;
     @UI("#calculate-button")
     Button calculateButton;
     @UI("[name=custom_radio_odd]")
@@ -48,20 +56,46 @@ public class MetalsAndColorsForm extends Form<MetalsAndColors> {
 
     }
 
-    public void selectEvenNumbers(String value) {
-        evenNumbers.select(value);
+    public void selectEvenNumbers(List<Integer> value) {
+        for (Integer val : value) {
+            evenNumbers.select(val);
+        }
     }
 
     public void submit(MetalsAndColors data) {
-        //oddNumbers.select(data.getSummary().get(0));
-        //evenNumbers.select(data.getSummary().get(1));
-        //calculateButton.click();
         selectOddNumbers(data.getSummary());
+        selectEvenNumbers(data.getSummary());
         calculateButton.click();
         //elements.select(data.getElements().get(0));
         colors.expand();
         colors.select(data.getColor());
         metals.expand();
         metals.select(data.getMetals());
+        unselectOptionVegetables();
+        selectOptions(data.getVegetables());
+    }
+
+    private boolean vegetablesIsExpanded() {
+        return vegetablesIsExpanded.getAttribute("style").equals("display: block;");
+    }
+
+    public void selectOptions(List<String> value) {
+        for (String val : value) {
+            if (!vegetablesIsExpanded())
+                vegetablesExpand.click();
+            for (WebElement listOption : vegetablesList) {
+                if (listOption.getText().trim().equals(val))
+                    listOption.click();
+            }
+        }
+    }
+
+    public void unselectOptionVegetables() {
+        if (!vegetablesIsExpanded())
+            vegetablesExpand.click();
+        for (WebElement listOption : vegetablesList) {
+            if (listOption.getText().trim().equals("Vegetables"))
+                listOption.click();
+        }
     }
 }
